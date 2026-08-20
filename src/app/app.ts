22893b372d98f5506d37355fa74484918f418303
academic.cci@gmail.com <c-clarke@c-clarkes-Mac-mini.local>
2026-08-20 15:13:39 +0530

    layout complete

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FeatureComponent } from './components/feature/feature.component';
+import { ProductsComponent } from './components/products/products.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    SidebarComponent,
+  HeroComponent,
+  FeatureComponent,
+  ProductsComponent,
+  FooterComponent],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
